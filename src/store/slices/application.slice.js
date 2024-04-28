5983ba064b2fec12fa795cb56216f3f69404e1a1
gabecpf97 <gabrielcpf2997@gmail.com>
2024-04-28 15:48:08 -0700

    added store to react so we can start adding slice for features

---
+++ src/store/slices/application.slice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const applicationSlice = createSlice({
+  name: "application",
+  initialState: {},
+  reducers: {},
+});
+
+export default applicationSlice;
